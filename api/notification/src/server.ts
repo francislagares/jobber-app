d9e01db0a9efdd3496f74f3ad25d198f52014945
Francis Lagares <francis.lagares@gmail.com>
2024-02-05 19:41:26 +0100

    feat: notification server startup

---
+++ api/notification/src/server.ts
@@ -0,0 +1,32 @@
+import http from 'http';
+
+import { winstonLogger } from '@francislagares/jobber-shared';
+import { config } from '@notification/config';
+import { Application } from 'express';
+import { Logger } from 'winston';
+
+const SERVER_PORT = 4001;
+const logger: Logger = winstonLogger(
+  `${config.ELASTIC_SEARCH_URL}`,
+  'notificationServer',
+  'debug',
+);
+
+export const start = (app: Application): void => {
+  startServer(app);
+};
+
+export const startServer = (app: Application): void => {
+  try {
+    const httpServer: http.Server = new http.Server(app);
+
+    logger.info(
+      `Worker with process id of ${process.pid} on notification server has started`,
+    );
+    httpServer.listen(SERVER_PORT, () => {
+      logger.info(`Notification server running on port ${SERVER_PORT}`);
+    });
+  } catch (error) {
+    logger.log('error', 'NotificationService startServer() method', error);
+  }
+};
