0fdb561fa0d558925c366c5e6b1b9cd42ba3b75b
Francis Lagares <francis.lagares@gmail.com>
2024-02-12 14:19:03 +0100

    test: order consumer unit test

--- api/notification/src/queues/test/email.consumer.test.ts
+++ api/notification/src/queues/test/email.consumer.test.ts
@@ -1,5 +1,8 @@
 import * as connection from '@notification/queues/connection';
-import { consumeAuthEmailMessage } from '@notification/queues/email.consumer';
+import {
+  consumeAuthEmailMessage,
+  consumeOrderEmailMessage,
+} from '@notification/queues/email.consumer';
 import { Channel } from 'amqplib';
 import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
 
@@ -50,6 +53,44 @@ describe('Email Consumer', () => {
         'auth-email-queue',
         'jobber-email-notification',
         'auth-email',
+      );
+    });
+  });
+
+  describe('consumeOrderEmailMessage method', () => {
+    test('should connect to channel and create a queue', async () => {
+      const channel = {
+        assertExchange: vi.fn(),
+        assertQueue: vi.fn(),
+        bindQueue: vi.fn(),
+        publish: vi.fn(),
+        consume: vi.fn(),
+      };
+
+      vi.spyOn(connection, 'createConnection').mockReturnValue(
+        channel as never,
+      );
+      vi.spyOn(channel, 'assertExchange');
+      vi.spyOn(channel, 'assertQueue').mockReturnValue({
+        queue: 'order-email-queue',
+        messageCount: 0,
+        consumerCount: 0,
+      });
+
+      const connectionChannel: Channel | undefined =
+        await connection.createConnection();
+
+      await consumeOrderEmailMessage(connectionChannel);
+
+      expect(connectionChannel.assertExchange).toHaveBeenCalledWith(
+        'jobber-order-notification',
+        'direct',
+      );
+      expect(connectionChannel.assertQueue).toHaveBeenCalledTimes(1);
+      expect(connectionChannel.bindQueue).toHaveBeenCalledWith(
+        'order-email-queue',
+        'jobber-order-notification',
+        'order-email',
       );
     });
   });
