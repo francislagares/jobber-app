1632e1deff6e664149862068aacb5bbdbd20f074
Francis Lagares <francis.lagares@gmail.com>
2024-02-17 21:03:47 +0100

    feat: implement authentication middleware

---
+++ api/gateway/src/middleware/auth.ts
@@ -0,0 +1,46 @@
+import {
+  AuthPayload,
+  BadRequestError,
+  NotAuthorizedError,
+} from '@francislagares/jobber-shared';
+import { config } from '@gateway/config';
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+
+class AuthMiddleware {
+  public verifyUser(req: Request, _res: Response, next: NextFunction) {
+    if (!req.session?.jwt) {
+      throw new NotAuthorizedError(
+        'Token is not available. Please log in again.',
+        'Gateway Service verifyUser() method error',
+      );
+    }
+
+    try {
+      const payload: AuthPayload = verify(
+        req.session.jwt,
+        `${config.JWT_TOKEN}`,
+      ) as AuthPayload;
+
+      req.currentUser = payload;
+    } catch (error) {
+      throw new NotAuthorizedError(
+        'Token is not available. Please log in again.',
+        'GatewayService verifyUser() method invalid session error',
+      );
+    }
+    next();
+  }
+
+  public checkAuthentication(req: Request, _res: Response, next: NextFunction) {
+    if (!req.currentUser) {
+      throw new BadRequestError(
+        'Authentication is required to access this route.',
+        'GatewayService verifyUser() method error',
+      );
+    }
+    next();
+  }
+}
+
+export const authMiddleware: AuthMiddleware = new AuthMiddleware();
