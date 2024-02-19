71e96b1871672ecd418e414cbf64cdf358210acb
Francis Lagares <francis.lagares@gmail.com>
2024-02-19 21:03:24 +0100

    feat: implement database connection class

---
+++ api/authentication/src/config/database.ts
@@ -0,0 +1,63 @@
+import { config } from '@authentication/config';
+import { winstonLogger } from '@francislagares/jobber-shared';
+import { PrismaClient } from '@prisma/client';
+import { Logger } from 'winston';
+
+const logger: Logger = winstonLogger(
+  `${config.ELASTIC_SEARCH_URL}`,
+  'authDatabaseServer',
+  'debug',
+);
+
+export class MySqlDBInstance {
+  private static instance: MySqlDBInstance;
+  private database: PrismaClient;
+
+  private constructor() {
+    this.database = new PrismaClient();
+    this.connectToDatabase();
+  }
+
+  public static async getInstance() {
+    if (!this.instance) {
+      this.instance = new MySqlDBInstance();
+    }
+    return this.instance;
+  }
+
+  private async connectToDatabase(): Promise<void> {
+    try {
+      await this.database.$connect();
+
+      logger.info(
+        'Authentication MySQL database connection has been successfully established!',
+      );
+    } catch (error) {
+      logger.error('Authentication Service - Unable to connect to database.');
+      logger.log(
+        'error',
+        'Authentication Service connectToDatabase() method:',
+        error,
+      );
+
+      await this.disconnectFromDatabase();
+
+      process.exit(1);
+    }
+  }
+
+  private async disconnectFromDatabase(): Promise<void> {
+    try {
+      await this.database.$disconnect();
+      logger.error('Authentication Service - Disconnected from database.');
+    } catch (error) {
+      logger.error(`Error disconnecting from database: ${error}`);
+    }
+  }
+
+  public async close(): Promise<void> {
+    await this.disconnectFromDatabase();
+
+    MySqlDBInstance.instance = null;
+  }
+}
