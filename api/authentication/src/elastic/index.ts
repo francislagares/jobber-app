5e0f4fdba12a9cc46d122a92cecde107cb17b129
Francis Lagares <francis.lagares@gmail.com>
2024-02-19 21:05:24 +0100

    feat: create elasticsearch connection

---
+++ api/authentication/src/elastic/index.ts
@@ -0,0 +1,32 @@
+import { config } from '@authentication/config';
+import { Client } from '@elastic/elasticsearch';
+import { ClusterHealthResponse } from '@elastic/elasticsearch/lib/api/types';
+import { winstonLogger } from '@francislagares/jobber-shared';
+import { Logger } from 'winston';
+
+const logger: Logger = winstonLogger(
+  `${config.ELASTIC_SEARCH_URL}`,
+  'authElasticSearchServer',
+  'debug',
+);
+
+const elasticSearchClient = new Client({
+  node: `${config.ELASTIC_SEARCH_URL}`,
+});
+
+export async function checkConnection(): Promise<void> {
+  let isConnected = false;
+
+  while (!isConnected) {
+    try {
+      const health: ClusterHealthResponse =
+        await elasticSearchClient.cluster.health({});
+
+      logger.info(`AuthService Elasticsearch health status - ${health.status}`);
+      isConnected = true;
+    } catch (error) {
+      logger.error('Connection to Elasticsearch failed. Retrying...');
+      logger.log('error', 'AuthService checkConnection() method:', error);
+    }
+  }
+}
