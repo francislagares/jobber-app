c0906f87fb5f6b2f33a80e2beeeda193f19eae9a
Francis Lagares <francis.lagares@gmail.com>
2024-07-27 17:22:12 +0200

    feat(users): define base routes

--- api/users/src/routes/health.ts
+++ api/users/src/routes/health.ts
@@ -1,9 +1,11 @@
 import express, { Router } from 'express';
 
+import { health } from '@users/controllers/health';
+
 const router: Router = express.Router();
 
 export const healthRoutes = (): Router => {
-  router.get('/user-health', () => console.log('TODO route'));
+  router.get('/user-health', health);
 
   return router;
 };
--- api/users/src/routes/index.ts
+++ api/users/src/routes/index.ts
@@ -1,10 +1,16 @@
 import { Application } from 'express';
 
+import { verifyGatewayRequest } from '@francislagares/jobber-shared';
+
 const BUYER_BASE_PATH = '/api/v1/buyer';
 const SELLER_BASE_PATH = '/api/v1/seller';
 
 export const appRoutes = (app: Application): void => {
-  app.use('', () => console.log('TODO routes'));
-  app.use(BUYER_BASE_PATH, () => console.log('TODO routes'));
-  app.use(SELLER_BASE_PATH, () => console.log('TODO routes'));
+  app.use('', () => console.log('health routes'));
+  app.use(BUYER_BASE_PATH, verifyGatewayRequest, () =>
+    console.log('buyer routes'),
+  );
+  app.use(SELLER_BASE_PATH, verifyGatewayRequest, () =>
+    console.log('seller routes'),
+  );
 };
