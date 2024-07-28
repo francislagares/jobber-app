904368d05e899bd3deedf26ab06038a61b194c6b
Francis Lagares <francis.lagares@gmail.com>
2024-07-28 11:19:01 +0200

    feat: switch connection database with mongoose

--- api/users/src/config/database.ts
+++ api/users/src/config/database.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import mongoose from 'mongoose';
 import { Logger } from 'winston';
 
 import { winstonLogger } from '@francislagares/jobber-shared';
@@ -12,6 +12,55 @@ const logger: Logger = winstonLogger(
 );
 
 export class MongoDBInstance {
+  private static instance: MongoDBInstance;
+
+  private constructor() {
+    this.getDbConnection();
+  }
+
+  public static getInstance() {
+    if (!this.instance) {
+      this.instance = new MongoDBInstance();
+    }
+    return this.instance;
+  }
+
+  private async getDbConnection() {
+    try {
+      // checks if mongoose is not connected
+      if (mongoose.connection.readyState === 0) {
+        await mongoose.connect(config.DATABASE_URL);
+        logger.info(
+          'Users MongoDB database connection has been successfully established!',
+        );
+      }
+    } catch (error) {
+      logger.error('Users Service - Unable to connect to database.');
+      logger.log('error', 'Users Service connectToDatabase() method:', error);
+
+      await this.disconnectFromDatabase();
+
+      process.exit(1);
+    }
+  }
+
+  private async disconnectFromDatabase(): Promise<void> {
+    try {
+      await mongoose.disconnect();
+      logger.error('Users Service - Disconnected from database.');
+    } catch (error) {
+      logger.error(`Error disconnecting from database: ${error}`);
+    }
+  }
+
+  public async close(): Promise<void> {
+    await this.disconnectFromDatabase();
+
+    MongoDBInstance.instance = null;
+  }
+}
+
+/* export class MongoDBInstance {
   private static instance: MongoDBInstance;
   private database: PrismaClient;
 
@@ -59,3 +108,4 @@ export class MongoDBInstance {
     MongoDBInstance.instance = null;
   }
 }
+ */
