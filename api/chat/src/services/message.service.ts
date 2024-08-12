040268928984d07b4772c5203520873e72dc6f35
Francis Lagares <francis.lagares@gmail.com>
2024-08-12 18:46:08 +0200

    feat: implement chat service get methods

--- api/chat/src/services/message.service.ts
+++ api/chat/src/services/message.service.ts
@@ -1,4 +1,5 @@
 import {
+  ConversationDocument,
   MessageDetails,
   MessageDocument,
   toLowerCase,
@@ -51,4 +52,86 @@ export const addMessage = async (
   socketIOChatObject.emit('message received', message);
 
   return message;
+};
+
+export const getConversation = async (
+  sender: string,
+  receiver: string,
+): Promise<ConversationDocument[]> => {
+  const query = {
+    $or: [
+      { senderUsername: sender, receiverUsername: receiver },
+      { senderUsername: receiver, receiverUsername: sender },
+    ],
+  };
+  const conversation: ConversationDocument[] =
+    await ConversationModel.aggregate([{ $match: query }]);
+
+  return conversation;
+};
+
+export const getUserConversationList = async (
+  username: string,
+): Promise<MessageDocument[]> => {
+  const query = {
+    $or: [{ senderUsername: username }, { receiverUsername: username }],
+  };
+  const messages: MessageDocument[] = await MessageModel.aggregate([
+    { $match: query },
+    {
+      $group: {
+        _id: '$conversationId',
+        result: { $top: { output: '$$ROOT', sortBy: { createdAt: -1 } } },
+      },
+    },
+    {
+      $project: {
+        _id: '$result._id',
+        conversationId: '$result.conversationId',
+        sellerId: '$result.sellerId',
+        buyerId: '$result.buyerId',
+        receiverUsername: '$result.receiverUsername',
+        receiverPicture: '$result.receiverPicture',
+        senderUsername: '$result.senderUsername',
+        senderPicture: '$result.senderPicture',
+        body: '$result.body',
+        file: '$result.file',
+        gigId: '$result.gigId',
+        isRead: '$result.isRead',
+        hasOffer: '$result.hasOffer',
+        createdAt: '$result.createdAt',
+      },
+    },
+  ]);
+
+  return messages;
+};
+
+export const getMessages = async (
+  sender: string,
+  receiver: string,
+): Promise<MessageDocument[]> => {
+  const query = {
+    $or: [
+      { senderUsername: sender, receiverUsername: receiver },
+      { senderUsername: receiver, receiverUsername: sender },
+    ],
+  };
+  const messages: MessageDocument[] = await MessageModel.aggregate([
+    { $match: query },
+    { $sort: { createdAt: 1 } },
+  ]);
+
+  return messages;
+};
+
+export const getUserMessages = async (
+  messageConversationId: string,
+): Promise<MessageDocument[]> => {
+  const messages: MessageDocument[] = await MessageModel.aggregate([
+    { $match: { conversationId: messageConversationId } },
+    { $sort: { createdAt: 1 } },
+  ]);
+
+  return messages;
 };
