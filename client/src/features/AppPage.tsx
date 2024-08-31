fa729d6582270eee254e6dd9764f568533700179
Francis Lagares <francis.lagares@gmail.com>
2024-08-31 19:23:45 +0200

    feat: create AppRouter component

---
+++ client/src/features/AppPage.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const AppPage: FC = () => {
+  return <div>AppPage</div>;
+};
+
+export default AppPage;
---
+++ client/src/routes/AppRouter.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+
+import { RouteObject, useRoutes } from 'react-router-dom';
+
+import AppPage from '@/features/AppPage';
+import Home from '@/features/home/Home';
+
+const AppRouter: FC = () => {
+  const routes: RouteObject[] = [
+    {
+      path: '/',
+      element: <AppPage />,
+    },
+    {
+      path: '/',
+      element: <Home />,
+    },
+  ];
+
+  return useRoutes(routes);
+};
+
+export default AppRouter;
