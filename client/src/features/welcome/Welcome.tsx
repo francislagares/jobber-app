2fe9adf53a3d51f16833c6d27f51ade038bfbbd3
Francis Lagares <francis.lagares@gmail.com>
2024-09-03 20:12:25 +0200

    feat: create GigTabs component

--- client/src/features/welcome/Welcome.tsx
+++ client/src/features/welcome/Welcome.tsx
@@ -2,10 +2,14 @@ import { FC, lazy, LazyExoticComponent, Suspense } from 'react';
 
 import { IHeader } from '@/shared/header/interfaces/header.interface';
 
-import Hero from './Hero';
-
 const WelcomeHeader: LazyExoticComponent<FC<IHeader>> = lazy(
-  () => import('src/shared/header/components/Header'),
+  () => import('@/shared/header/components/Header'),
+);
+const Hero: LazyExoticComponent<FC> = lazy(
+  () => import('@/features/welcome/Hero'),
+);
+const GigTabs: LazyExoticComponent<FC> = lazy(
+  () => import('@/features/welcome/gig-tabs/GigTabs'),
 );
 
 const Welcome: FC = () => {
@@ -14,6 +18,7 @@ const Welcome: FC = () => {
       <Suspense>
         <WelcomeHeader navClass="navbar peer-checked:navbar-active fixed z-20 w-full border-b border-gray-100 bg-white/90 shadow-2xl shadow-gray-600/5 backdrop-blur dark:border-gray-800 dark:bg-gray-900/80 dark:shadow-none" />
         <Hero />
+        <GigTabs />
       </Suspense>
     </div>
   );
